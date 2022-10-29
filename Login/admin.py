8f7a81cff081015dab9c81c76b33e426420c7efa
mox56 <mouaiad71@outlook.com>
2022-10-29 20:39:43 +0200

    database

--- Login/admin.py
+++ Login/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Department)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(ExamResult)
--- Login/models.py
+++ Login/models.py
@@ -1,3 +1,44 @@
+from random import choices
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    Code = models.CharField(max_length=10, null=True)
+    name = models.CharField(max_length=100, null=True)    
+    
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    index = models.IntegerField( null=True)
+    name = models.CharField(max_length=100, null=True )
+    Department = models.ManyToManyField(Department)
+    Semester = models.IntegerField(null=True)   
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
+    
+    def __str__(self):
+        return self.name
+    
+class Course(models.Model):
+    Code = models.CharField(max_length=10, null=True)
+    name =models.CharField(max_length=100, null=True)
+    Department = models.ManyToManyField(Department)
+    Semester = models.IntegerField(null=True)   
+    
+    def __str__(self):
+        return self.name
+
+
+    
+class ExamResult(models.Model):
+    STATUS = (
+        ('A','A'), ('B','B'),('C','C'),('D','D'),('F','F'),('Z','Z')
+    )
+    student_index= models.ForeignKey(Student, null=True, on_delete= models.SET_NULL)
+    Course_code= models.ForeignKey(Course, null=True, on_delete= models.SET_NULL)
+    Mark=models.CharField( max_length=50, null=True, choices=STATUS)
+    Semester= models.IntegerField(null=True) 
+    
+    def __str__(self):
+        return self.Mark
